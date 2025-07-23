6c99f355aa9b94ab9e5233d8252374b9dcb9f855
chinmaye <chinmayegowda@proton.me>
2025-07-24 01:56:37 +0530

    updated cors url

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://parallelai-henna.vercel.app'],
     credentials: true
 }));
+
 app.use(express.json());
 
 // Routes
